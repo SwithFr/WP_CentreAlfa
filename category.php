1dc2d222f188a25d0cccd04353ed8a282ce2e15c
SwithFr <contact@swith.fr>
2015-10-09 20:05:52 +0200

    add category template

--- category.php
+++ category.php
@@ -1,16 +1,25 @@
-<?php get_header(); ?>
-    <h1>Salut les Steaks !</h1>
-
-    <section>
-        <h2>Category</h2>
-        <?php if (have_posts()) : ?>
-            <?php while (have_posts()) : the_post(); ?>
-                <arcticle id="<?php the_ID(); ?>">
-                    <h3><?php the_title(); ?></h3>
-                    <div class="content">
-                        <?php the_content(); ?>
-                    </div>
-                </arcticle>
+<?php get_header( 'category' ); ?>
+    <section class="wrapper">
+        <h2><?php single_cat_title( '', true ); ?></h2>
+        <?php wp_nav_menu([
+            'theme_location'  => 'services-menu',
+            'container'       => 'ul',
+            'menu_class'      => 'nav nav--bordered',
+            'echo'            => true,
+            'items_wrap'      => '<ul class="%2$s">%3$s</ul>'
+        ]); ?>
+        <?php $query = new WP_Query([
+            'post_type' => 'sections',
+            'cat' => $cat
+        ]); ?>
+        <?php if ($query->have_posts()) : ?>
+            <?php while ($query->have_posts()) : $query->the_post(); ?>
+               <article>
+                   <h3><?php the_title(); ?></h3>
+                   <div class="content">
+                       <?php the_content(); ?>
+                   </div>
+               </article>
             <?php endwhile; ?>
         <?php endif; ?>
     </section>
