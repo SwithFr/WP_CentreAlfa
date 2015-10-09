a3ce57cdb7e755f72697f47ff5692385fb1943d7
SwithFr <contact@swith.fr>
2015-10-09 20:07:09 +0200

    update fix document outline

--- index.php
+++ index.php
@@ -76,7 +76,7 @@
                             <p><?= $location['address']; ?></p>
                         </div>
                         <div class="address__block">
-                            <h3>Coordonnées</h3>
+                            <p class="address__title">Coordonnées</p>
                             <p>Tel : <?php the_field('telephone') ?></p>
                             <p>Fax : <?php the_field('fax'); ?></p>
                             <p class="horaires">Ouvert du lundi au vendredi de 9h à 18h</p>
@@ -85,20 +85,18 @@
                 <?php endwhile; ?>
             <?php endif; ?>
             <div class="address__emails">
-                <h3>Emails des services</h3>
+                <p class="address__title">Emails des services</p>
+                <?php $query = new WP_Query([
+                    'post_type' => 'emails',
+                    'order' => 'ASC'
+                ]); ?>
                 <dl>
-                    <dt class="address__service-name">Thérapeutique :</dt>
-                    <dd class="address__emails__item">alfa.therapeutique@gmail.com</dd>
-                    <dt class="address__service-name">Prévention : </dt>
-                    <dd class="address__emails__item">alfa.prevention.gmail</dd>
-                    <dt class="address__service-name">Parentatlité : </dt>
-                    <dd class="address__emails__item">alfa.parentalite@gmail.com</dd>
-                    <dt class="address__service-name">Social : </dt>
-                    <dd class="address__emails__item">alfa.servicesocial@gmail.com</dd>
-                    <dt class="address__service-name">Direction : </dt>
-                    <dd class="address__emails__item">alfa.dungelhoeff@gmail.com</dd>
-                    <dt class="address__service-name">Administration - compatibilité : </dt>
-                    <dd class="address__emails__item">alfa.yvettepetit@gmail.com</dd>
+                    <?php if ($query->have_posts()) : ?>
+                        <?php while ($query->have_posts()) : $query->the_post(); ?>
+                            <dt class="address__service-name"><?php the_title(); ?> :</dt>
+                            <dd class="address__emails__item"><?php the_field('email'); ?></dd>
+                        <?php endwhile; ?>
+                    <?php endif; ?>
                 </dl>
             </div>
         </section>
@@ -116,7 +114,7 @@
                             <p><?= $location['address']; ?></p>
                         </div>
                         <div class="address__block">
-                            <h3>Coordonnées</h3>
+                            <p class="address__title">Coordonnées</p>
                             <p>Tel / Fax : <?php the_field('fax'); ?></p>
                             <p>GSM : <?php the_field('telephone') ?></p>
                             <p><?= the_field('email'); ?></p>
@@ -131,122 +129,5 @@
     </section>
     <script src="https://maps.googleapis.com/maps/api/js?v=3.exp&sensor=false"></script>
     <script src="<?= get_template_directory_uri(); ?>/js/jquery.js"></script>
-    <script type="text/javascript">
-        (function($) {
-
-            function new_map( $el ) {
-
-                // var
-                var $markers = $el.find('.marker');
-
-
-                // vars
-                var args = {
-                    zoom		: 16,
-                    center		: new google.maps.LatLng(0, 0),
-                    mapTypeId	: google.maps.MapTypeId.ROADMAP
-                };
-
-
-                // create map
-                var map = new google.maps.Map( $el[0], args);
-
-
-                // add a markers reference
-                map.markers = [];
-
-
-                // add markers
-                $markers.each(function(){
-
-                    add_marker( $(this), map );
-
-                });
-
-
-                // center map
-                center_map( map );
-
-
-                // return
-                return map;
-
-            }
-
-            function add_marker( $marker, map ) {
-
-                // var
-                var latlng = new google.maps.LatLng( $marker.attr('data-lat'), $marker.attr('data-lng') );
-
-                // create marker
-                var marker = new google.maps.Marker({
-                    position	: latlng,
-                    map			: map
-                });
-
-                // add to array
-                map.markers.push( marker );
-
-                // if marker contains HTML, add it to an infoWindow
-                if( $marker.html() )
-                {
-                    // create info window
-                    var infowindow = new google.maps.InfoWindow({
-                        content		: $marker.html()
-                    });
-
-                    // show info window when marker is clicked
-                    google.maps.event.addListener(marker, 'click', function() {
-
-                        infowindow.open( map, marker );
-
-                    });
-                }
-
-            }
-
-            function center_map( map ) {
-
-                // vars
-                var bounds = new google.maps.LatLngBounds();
-
-                // loop through all markers and create bounds
-                $.each( map.markers, function( i, marker ){
-
-                    var latlng = new google.maps.LatLng( marker.position.lat(), marker.position.lng() );
-
-                    bounds.extend( latlng );
-
-                });
-
-                // only 1 marker?
-                if( map.markers.length == 1 )
-                {
-                    // set center of map
-                    map.setCenter( bounds.getCenter() );
-                    map.setZoom( 16 );
-                }
-                else
-                {
-                    // fit to bounds
-                    map.fitBounds( bounds );
-                }
-
-            }
-
-            var map = null;
-
-            $(document).ready(function(){
-
-                $('.address__map').each(function(){
-
-                    // create map
-                    map = new_map( $(this) );
-
-                });
-
-            });
-
-        })(jQuery);
-    </script>
+    <script src="<?= get_template_directory_uri(); ?>/js/maps.js"></script>
 <?php get_footer(); ?>
