d08f5fe03b23a27d80fbd7fa0d12b0e8c6d4e721
SwithFr <contact@swith.fr>
2015-10-14 17:06:02 +0200

    update markup

--- page-a-propos.php
+++ page-a-propos.php
@@ -1,9 +1,11 @@
 <?php get_header( 'simple' ); ?>
-    <section class="wrapper">
+    <section class="wrapper content">
         <?php if (have_posts()) : ?>
             <?php while (have_posts()) : the_post(); ?>
-                <h2><?php the_title(); ?></h2>
-                <?php the_content(); ?>
+                <div class="content__item">
+                    <h2 class="content__item__title"><?php the_title(); ?></h2>
+                    <?php the_content(); ?>
+                </div>
             <?php endwhile; ?>
         <?php endif; ?>
     </section>
