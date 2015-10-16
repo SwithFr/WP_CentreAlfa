a196d1e285dee9c89a897cf400333b8d85599c19
SwithFr <contact@swith.fr>
2015-10-16 19:02:33 +0200

    add class link__name

--- page-liens.php
+++ page-liens.php
@@ -9,7 +9,7 @@
         <?php if ($query->have_posts()) : ?>
             <?php while ($query->have_posts()) : $query->the_post(); ?>
                 <div class="content__item link">
-                    <a class="content__item__title" href="<?php the_field('lien'); ?>"><?php the_title(); ?></a>
+                    <a class="content__item__title link__name" href="<?php the_field('lien'); ?>"><?php the_title(); ?></a>
                     <div class="content__item__content"><?php the_field('description'); ?></div>
                 </div>
             <?php endwhile; ?>
