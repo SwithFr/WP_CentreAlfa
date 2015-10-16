041c5b29f29a145e5d7a39a812498fedbf2724db
SwithFr <contact@swith.fr>
2015-10-16 19:02:46 +0200

    update search template

--- search.php
+++ search.php
@@ -1,32 +1,77 @@
 <?php get_header( 'simple' ); ?>
     <section class="wrapper content">
+        <h2>Résultats de la recherche <span class="searchQuery"><?= get_search_query(); ?></span></h2>
         <?php if (have_posts()) : ?>
             <?php while (have_posts()) : the_post(); ?>
                 <?php if($post->post_type === "emails"): ?>
-                    <p class="result__type">Emails :</p>
+                    <?php if(!$eDisplayed): ?>
+                        <p class="result__type">Emails :</p>
+                        <?php $eDisplayed = true; ?>
+                    <?php endif; ?>
+
+                    <section class="content__item address__emails">
+                        <h3 class="content__item__title"><?php the_title(); ?></h3>
+                        <p class="content__item__content"><?php the_field('email'); ?></p>
+                    </section>
                 <?php endif; ?>
                 
                 <?php if($post->post_type === "services"): ?>
-                    <p class="result__type">Services :</p>
+                    <?php if(!$sDisplayed): ?>
+                        <p class="result__type">Services :</p>
+                        <?php $sDisplayed = true; ?>
+                    <?php endif; ?>
+
+                    <section class="content__item">
+                        <h3 class="content__item__title"><?php the_title(); ?></h3>
+                        <p class="content__item__content"><?php the_content(); ?></p>
+                    </section>
                 <?php endif; ?>
 
                 <?php if($post->post_type === "addresses"): ?>
-                    <p class="result__type">Adresses :</p>
+                    <?php if(!$aDisplayed): ?>
+                        <p class="result__type">Adresses :</p>
+                        <?php $aDisplayed = true; ?>
+                    <?php endif; ?>
+
+                    <section class="content__item">
+                        <h3 class="content__item__title"><?php the_title(); ?></h3>
+                        <div class="content__item__content">
+                            <p><?php the_field('adresse'); ?></p>
+                            <p><?php the_field('telephone'); ?></p>
+                        </div>
+                    </section>
                 <?php endif; ?>
 
                 <?php if($post->post_type === "membres"): ?>
-                    <p class="result__type">Membres :</p>
+                    <?php if(!$mDisplayed): ?>
+                        <p class="result__type">Membres :</p>
+                        <?php $mDisplayed = true; ?>
+                    <?php endif; ?>
+
+                    <section class="content__item">
+                        <h3 class="content__item__title"><?php the_field('name'); ?></h3>
+                        <div class="content__item__content">
+                            <p><?php the_field('fonction'); ?></p>
+                        </div>
+                    </section>
                 <?php endif; ?>
 
                 <?php if($post->post_type === "page"): ?>
-                    <p class="result__type">Pages :</p>
-                <?php endif; ?>
+                    <?php if(!$pDisplayed): ?>
+                        <p class="result__type">Pages :</p>
+                        <?php $pDisplayed = true; ?>
+                    <?php endif; ?>
 
-                <div class="content__item">
-                    <h2 class="content__item__title"><?php the_title(); ?></h2>
-                    <?php the_content(); ?>
-                </div>
+                    <section class="content__item">
+                        <h3 class="content__item__title"><?php the_title(); ?></h3>
+                        <div class="content__item__content">
+                            <p><?= wp_trim_words( the_content(), 30, '...' ); ?></p>
+                        </div>
+                    </section>
+                <?php endif; ?>
             <?php endwhile; ?>
+        <?php else: ?>
+            <p class="error">Aucun résultat</p>
         <?php endif; ?>
     </section>
 <?php get_footer(); ?>
